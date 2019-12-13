42ea13381d05b5e16fc2b121386464ed27674aaa
mahnazzzz <mahnaazi@yahoo.com>
2019-12-13 06:29:27 +0100

    ordre class

--- pizzaMarioTestAssignment_6/src/main/java/Order.java
+++ pizzaMarioTestAssignment_6/src/main/java/Order.java
@@ -9,5 +9,53 @@
  * @author Bruger
  */
 public class Order {
+    int id; 
+    String name;
+    String description;
+    int price;
+
+    public Order(String name, String description, int price) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" + "id=" + id + ", name=" + name + ", description=" + description + ", price=" + price + '}';
+    }
+    
     
 }
